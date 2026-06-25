bbd8e55e86555c3721f52513724936a0b404301f
HiteshTomar2004 <hitesh28tomar@gmail.com>
2026-06-25 15:29:25 +0530

    Preparing for preview

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,15 @@
-import Navbar from "./components/Navbar/Navbar";
-import Hero from "./sections/Hero/Hero";
+
 import About from "./sections/About/About";
-import Timeline from "./sections/Timeline/Timeline";
-import WaveIn from "./sections/WaveIn/WaveIn";
-import PrizePool from "./sections/PrizePool/PrizePool";
-import Registration from "./sections/Registration/Registration";
+
 import SponsorsMentors from "./sections/SponsorsMentors/SponsorsMentors";
-import FAQ from "./sections/FAQ/FAQ";
-import FooterSection from "./sections/FooterSection/FooterSection";
+
 
 
 function App() {
   return (
     <>
-      <Navbar />
-
-      <Hero />
       <About />
-      <WaveIn />
-      <Timeline />
-      <PrizePool />
-      <Registration />
       <SponsorsMentors />
-      <FAQ />
-      <FooterSection />
     </>
   );
 }
